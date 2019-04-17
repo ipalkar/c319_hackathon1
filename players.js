3cf295dee3e598bb58d4edf95edd96aded8372e9
Phong Truong <phongoli0@gmail.com>
2019-04-17 11:40:00 -0700

    added players and methods

---
+++ players.js
@@ -0,0 +1,36 @@
+class Players {
+    constructor(playerSpot, name) {
+        this.playerSpot = playerSpot;
+        this.name = name;
+        this.health = 10;
+        this.victoryPoints = 0;
+        this.inTokyo = false;
+    }
+
+    healthUp(num) {
+        this.health + num;
+        return this.health;
+    }
+
+    healthDown(num) {
+        this.health - num;
+        return this.health;
+    }
+
+    victoryPointsUp(num) {
+        this.victoryPoints + num;
+        return this.victoryPoints;
+    }
+
+    goIntoTokyo() {
+        this.inTokyo = true;
+        this.victoryPointsUp(1);
+        return this.inTokyo;
+    }
+
+    leaveTokyo() {
+        this.inTokyo = false;
+        return this.inTokyo;
+    }
+}
+
